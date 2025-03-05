f4fc46203caae09c88553722c29e6c34abfb9242
Mohammed Sami <mohammedsamiawad20@gmail.com>
2025-03-05 12:56:52 +0300

    Revert "issue fix"
    This reverts commit 6607b82602fbb32068f2576e6c6f9b01b2024c42.

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,11 +1,14 @@
-import {BsFlagFill } from "react-icons/bs";
+import { useContext } from "react";
+import { DarkModeContext } from "../component/DarkToggle";
+import { BsFillMoonStarsFill , BsFillFlagFill, BsFlagFill } from "react-icons/bs";
 
 export default function NotFound(){
-    
+    const { darkMode, setDarkMode } = useContext(DarkModeContext);
 
     return(
         <div className="min-h-screen">
-        
+        <BsFillMoonStarsFill onClick={() => setDarkMode(!darkMode)} className="text-violet-800 cursor-pointer duration-500 rounded-full border-violet-800 border-2 w-12 h-12 px-3 dark:hover:text-white hover:text-white hover:bg-violet-800 fixed bottom-5 right-5 dark:text-violet-800"/>                  
+        <div className={darkMode ? "dark bg-gray-900" : ""}>
                 <main className="px-10 container mx-auto font-mono dark:bg-gray-900 pt-16">
                     <div className="text-center">
                     <BsFlagFill className="text-violet-800 mx-auto text-5xl"/>
@@ -14,6 +17,6 @@ export default function NotFound(){
                     </div>
                 </main>
                 </div>
-                
+                </div>
     );
 }
